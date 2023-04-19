30c77d124c6cddf5aa140bd6eadbe5430985fa55
Izzat Alsharif <izzat5233@gmail.com>
2023-04-20 01:51:24 +0300

    sql files added

--- README.md
+++ README.md
@@ -23,14 +23,16 @@ A fictional coffee shop website, showcasing the services, products, and events o
 #### Installation
 1. Download and install XAMPP from the [official website](https://www.apachefriends.org/index.html).
 2. Clone or download the Cuppa Joy project from the repository.
-3. Place the project folder in the `htdocs` folder of your XAMPP installation (e.g., `C:/xampp/htdocs/` on Windows).
+3. Place the `cuppa_joy` folder in the `htdocs` folder of your XAMPP installation (e.g., `C:/xampp/htdocs/` on Windows).
 4. Start the Apache and MySQL services using the XAMPP Control Panel.
 5. Open your browser and go to `http://localhost/phpmyadmin`.
-6. Import and execute the `CREATE.sql` file located in the `cuppa_joy/sql` folder to set up the necessary tables.
+6. Import and execute the `CREATE.sql` file located in the `sql` folder to set up the necessary tables.
 7. Import and execute the `INSERT.sql` file from the same location to fill the tables with data
 
 #### Usage
-- Open your browser and go to `http://localhost/` and explore the website.
+- Open your browser and go to `http://localhost/cuppa_joy`. 
+- Explore the website.
 
 ## Credits
-[@izzat5233](https://github.com/izzat5233) & [@Sulaiman111](https://github.com/Sulaiman111)
+- [@izzat5233](https://github.com/izzat5233)
+- [@Sulaiman111](https://github.com/Sulaiman111)
---
+++ sql/INSERT.sql
@@ -0,0 +1,47 @@
+INSERT INTO coffee(
+    NAME,
+    description,
+    price,
+    image_url
+)
+VALUES(
+    "Espresso",
+    "Rich and bold, our espresso is the perfect pick-me-up.",
+    11.99,
+    "images/coffee/espresso.jpg"
+),(
+    "Americano",
+    "Our Americano is a smooth blend of espresso and hot water, offering a milder flavor for those who prefer a less intense coffee experience.",
+    11.99,
+    "images/coffee/americano.jpg"
+),(
+    "Cappuccino",
+    "A classic Italian favorite, our cappuccino combines espresso, steamed milk, and a delicate layer of frothy milk foam for a delightful balance of flavors.",
+    9.99,
+    "images/coffee/cappuccino.jpg"
+),(
+    "Latte",
+    "Our latte features a harmonious mix of espresso and steamed milk, creating a creamy and comforting beverage perfect for any time of day.",
+    10.99,
+    "images/coffee/latte.jpg"
+),(
+    "Mocha",
+    "Indulge in our mocha, a delectable blend of espresso, rich chocolate syrup, and steamed milk, topped with a dollop of whipped cream.",
+    10.99,
+    "images/coffee/mocha.jpg"
+),(
+    "Flat White",
+    "Originating in Australia and New Zealand, our flat white combines espresso with velvety steamed milk, resulting in a smooth and creamy texture.",
+    15.99,
+    "images/coffee/flat_white.jpg"
+),(
+    "Iced Coffee",
+    "Our refreshing iced coffee is brewed to perfection and served over ice for a cool and invigorating treat on a hot day.",
+    7.99,
+    "images/coffee/iced_coffee.jpg"
+),(
+    "Cold Brew",
+    "Steeped for hours in cold water, our cold brew coffee offers a smooth and flavorful taste with lower acidity, perfect for those who prefer a less bitter coffee experience.",
+    11.99,
+    "images/coffee/cold_brew.jpg"
+);
