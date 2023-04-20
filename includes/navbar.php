590b74db0c9366bca1c6ab14eced126b69e004ef
Izzat Alsharif <izzat5233@gmail.com>
2023-04-20 22:19:26 +0300

    directory fix

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,10 +1,10 @@
 <header>
-    <img src="/assets/images/icons/logo.png" alt="logo">
+    <img src="assets/images/icons/logo.png" alt="logo">
     <nav class="main-navbar">
-        <a href="/index.php">Home</a>
-        <a href="/menu.php">Menu</a>
-        <a href="/about.php">About</a>
-        <a href="/contact.php">Contact</a>
+        <a href="index.php">Home</a>
+        <a href="menu.php">Menu</a>
+        <a href="about.php">About</a>
+        <a href="contact.php">Contact</a>
     </nav>
     <nav class="tools-navbar">
 
