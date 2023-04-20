fdb9642a460615308d24cdf05d481262d6063d89
Izzat Alsharif <izzat5233@gmail.com>
2023-04-20 20:31:27 +0300

    all main files created

---
+++ cuppa_joy/index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css">
+    <title>Cuppa Joy</title>
+</head>
+<body>
+<header>
+    <img src="assets/images/icons/logo.png" alt="logo">
+    <nav class="main-navbar">
+        <a href="#">Home</a>
+        <a href="#">Menu</a>
+        <a href="#">About</a>
+        <a href="#">Contact</a>
+    </nav>
+    <nav class="tools-navbar">
+
+    </nav>
+</header>
+</body>
+</html>
