5cc6509c2a373a7e166d95c0dd9715964f360cc6
Izzat Alsharif <izzat5233@gmail.com>
2023-04-21 01:07:18 +0300

    organization

--- menu.php
+++ menu.php
@@ -5,6 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="assets/css/styles.css">
     <title>Cuppa Joy | Our Menu</title>
 </head>
 <body>
@@ -13,5 +14,6 @@
 
 </main>
 <?php include "includes/footer.php" ?>
+<script src="assets/js/main.js"></script>
 </body>
 </html>
