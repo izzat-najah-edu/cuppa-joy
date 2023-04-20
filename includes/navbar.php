ceebe9f7202f6ba145825dd221e4f1202f1a4dea
Izzat Alsharif <izzat5233@gmail.com>
2023-04-20 20:58:57 +0300

    directory change

---
+++ includes/navbar.php
@@ -0,0 +1,12 @@
+<header>
+    <img src="/assets/images/icons/logo.png" alt="logo">
+    <nav class="main-navbar">
+        <a href="/index.php">Home</a>
+        <a href="/menu.php">Menu</a>
+        <a href="/about.php">About</a>
+        <a href="/contact.php">Contact</a>
+    </nav>
+    <nav class="tools-navbar">
+
+    </nav>
+</header>
