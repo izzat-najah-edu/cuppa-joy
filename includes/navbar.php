f3b168618317138b015fd70b04ee259773431d78
Izzat Alsharif <izzat5233@gmail.com>
2023-04-20 23:25:12 +0300

    directory fix

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,10 +1,14 @@
+<?php
+const BASE_URL = "http://localhost/cuppa-joy/"
+?>
+
 <header>
-    <img src="assets/images/icons/logo.png" alt="logo">
+    <img src="<?php echo BASE_URL . 'assets/images/icons/logo.png' ?>" alt="logo">
     <nav class="main-navbar">
-        <a href="index.php">Home</a>
-        <a href="menu.php">Menu</a>
-        <a href="about.php">About</a>
-        <a href="contact.php">Contact</a>
+        <a href="<?php echo BASE_URL . 'index.php' ?>">Home</a>
+        <a href="<?php echo BASE_URL . 'menu.php' ?>">Menu</a>
+        <a href="<?php echo BASE_URL . 'about.php' ?>">About</a>
+        <a href="<?php echo BASE_URL . 'contact.php' ?>">Contact</a>
     </nav>
     <nav class="tools-navbar">
 
