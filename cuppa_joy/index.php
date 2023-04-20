efb171b4e9d59a678077c26a48921cb6eac68cc9
Izzat Alsharif <izzat5233@gmail.com>
2023-04-20 20:38:45 +0300

    main pages are outlined

--- cuppa_joy/index.php
+++ cuppa_joy/index.php
@@ -6,20 +6,13 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css">
-    <title>Cuppa Joy</title>
+    <title>Cuppa Joy | Home</title>
 </head>
 <body>
-<header>
-    <img src="assets/images/icons/logo.png" alt="logo">
-    <nav class="main-navbar">
-        <a href="#">Home</a>
-        <a href="#">Menu</a>
-        <a href="#">About</a>
-        <a href="#">Contact</a>
-    </nav>
-    <nav class="tools-navbar">
+<?php include "includes/navbar.php" ?>
+<main>
 
-    </nav>
-</header>
+</main>
+<?php include "includes/footer.php" ?>
 </body>
 </html>
