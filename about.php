170bfe6ab32d0e557b836284d81e72b6e7d589be
Izzat Alsharif <izzat5233@gmail.com>
2023-04-22 16:30:26 +0300

    added bootstrap support

--- about.php
+++ about.php
@@ -5,6 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="assets/css/styles.css">
     <title>Cuppa Joy | About</title>
 </head>
@@ -15,5 +16,6 @@
 </main>
 <?php include "includes/footer.php" ?>
 <script src="assets/js/main.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
