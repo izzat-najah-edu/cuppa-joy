e735126f16c361f49d86d1a640c3cf3e2b8f15b3
Izzat Alsharif <izzat5233@gmail.com>
2023-04-23 15:37:09 +0300

    navbar created

--- includes/header.php
+++ includes/header.php
@@ -1,12 +1,43 @@
 <header>
-    <img src="/cuppa-joy/assets/images/icons/logo.png" alt="logo">
-    <nav class="main-navbar">
-        <a href="/cuppa-joy/index.php">Home</a>
-        <a href="/cuppa-joy/menu.php">Menu</a>
-        <a href="/cuppa-joy/about.php">About</a>
-        <a href="/cuppa-joy/contact.php">Contact</a>
-    </nav>
-    <nav class="tools-navbar">
-
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarContent">
+                <i class="navbar-toggler-icon"></i>
+            </button>
+        </div>
+        <div id="navbarContent" class="collapse navbar-collapse">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="/cuppa-joy/index.php">
+                        <span class="display-6 d-inline-flex">
+                            <i class="bi bi-house"></i>Home
+                        </span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/cuppa-joy/menu.php">
+                        <span class="display-6 d-inline-flex">
+                            <i class="bi bi-list"></i>Menu
+                        </span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/cuppa-joy/about.php">
+                        <span class="display-6 d-inline-flex">
+                            <i class="bi bi-file-person"></i>About
+                        </span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/cuppa-joy/contact.php">
+                        <span class="display-6 d-inline-flex">
+                            <i class="bi bi-headset"></i>Contact
+                        </span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div class="container-fluid">
+        </div>
     </nav>
 </header>
