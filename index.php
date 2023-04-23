a5d406dd9eece1cb1f39c0aca9d195251571955b
Izzat Alsharif <izzat5233@gmail.com>
2023-04-23 18:56:42 +0300

    refactoring

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 $logo_url = "assets/images/icons/logo_animated.gif";
 $logo_title = "Cuppa Joy";
 $logo_description = "Your Home Away from Home for the Finest Coffee Experience";
-include "includes/header.php"
+include "includes/header.php";
 ?>
 <main class="p-5">
     <div class="p-5 container-fluid max-width-1200 bg-light rounded-3">
