57c646ae71d80bc0d76436113562c57fcf55de2c
Izzat Alsharif <izzat5233@gmail.com>
2023-04-29 15:35:06 +0300

    bug fix

--- contact.php
+++ contact.php
@@ -11,11 +11,11 @@
     <title>Cuppa Joy | Contact</title>
 </head>
 <body>
-<?php include "includes/header.php" ?>
+<?php include_once "includes/header.php" ?>
 <main>
 
 </main>
-<?php include "includes/footer.php" ?>
+<?php include_once "includes/footer.php" ?>
 <script src="assets/js/main.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
--- index.php
+++ index.php
@@ -11,7 +11,7 @@
     <title>Cuppa Joy | Home</title>
 </head>
 <body class="bg-my-primary-gradient">
-<?php include "includes/header.php"; ?>
+<?php include_once "includes/header.php"; ?>
 <main class="p-5">
     <div class="card max-width-400 bg-transparent border-0 text-light text-center">
         <img class="card-img-top" src="assets/images/icons/logo_animated.gif" alt="Logo">
@@ -113,7 +113,7 @@
         </section>
     </div>
 </main>
-<?php include "includes/footer.php" ?>
+<?php include_once "includes/footer.php" ?>
 <script src="assets/js/main.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
