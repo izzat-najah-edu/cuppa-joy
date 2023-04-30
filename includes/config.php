9bca1b558a8115e8517a55818c5baaa875f79834
Izzat Alsharif <izzat5233@gmail.com>
2023-04-30 21:51:10 +0300

    database configuration

--- includes/config.php
+++ includes/config.php
@@ -1,1 +1,23 @@
 <?php
+
+class Database
+{
+    private static ?mysqli $connection = null;
+
+    public static function getConnection(): mysqli
+    {
+        if (self::$connection === null) {
+            try {
+                self::$connection = new mysqli(
+                    'localhost',
+                    'root', '',
+                    'cuppa_joy_coffee_shop'
+                );
+            } catch (mysqli_sql_exception $e) {
+                echo 'Database Error: ' . $e->getMessage();
+                die();
+            }
+        }
+        return self::$connection;
+    }
+}
