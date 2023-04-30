8f1426b26ea0f3a20e849f909b5652729e2b8b20
Izzat Alsharif <izzat5233@gmail.com>
2023-04-30 22:08:36 +0300

    about content

--- about.php
+++ about.php
@@ -19,16 +19,71 @@ $footer = new Footer();
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.4/font/bootstrap-icons.css">
     <link rel="stylesheet" href="assets/css/styles.css">
+    <link rel="stylesheet" href="assets/css/about.css">
     <title>Cuppa Joy | About</title>
 </head>
-<body>
+<body class="bg-my-primary-gradient">
 <?php
 $header->open();
 $header->renderNavbar();
 $header->close();
 ?>
 <main>
-
+    <div class="wrapper">
+        <h1>Our Services</h1>
+        <p>At our coffee shop, we pride ourselves on delivering exceptional experiences for our customers. Our services
+            cater to diverse preferences and needs, ensuring that everyone feels welcome and satisfied.</p>
+        <div class="content-box">
+            <div class="card">
+                <i class='bx bxs-coffee-bean'></i>
+                <h2>Coffee & Craft</h2>
+                <p>
+                    We're passionate about providing high-quality, ethically sourced coffee to our customers. Our
+                    skilled baristas meticulously craft each cup, guaranteeing a delightful taste every time.
+                </p>
+            </div>
+            <div class="card">
+                <i class='bx bxs-group'></i>
+                <h2>Our Partners</h2>
+                <p>
+                    We collaborate with local farmers and suppliers to ensure we always use fresh, sustainable
+                    ingredients. Our partners share our commitment to quality and social responsibility.
+                </p>
+            </div>
+            <div class="card">
+                <i class='bx bxs-time-five'></i>
+                <h2>24/7 Call Center Services</h2>
+                <p>
+                    We understand that our customers may need assistance at any time. Our dedicated call center is
+                    available 24/7 to answer your questions and address any concerns.
+                </p>
+            </div>
+            <div class="card">
+                <i class='bx bx-pointer'></i>
+                <h2>Social Media Marketing</h2>
+                <p>
+                    Stay connected with us through our social media channels. We share updates, promotions, and engaging
+                    content to keep you informed and entertained.
+                </p>
+            </div>
+            <div class="card">
+                <i class='bx bxs-planet'></i>
+                <h2>Planet</h2>
+                <p>
+                    We're committed to being environmentally responsible. We strive to reduce our carbon footprint,
+                    minimize waste, and give back to the planet through sustainable practices.
+                </p>
+            </div>
+            <div class="card">
+                <i class='bx bxs-conversation'></i>
+                <h2>People</h2>
+                <p>
+                    Our employees, whom we call partners, are at the heart of our business. We invest in their
+                    well-being and personal growth, fostering a positive and inclusive work environment.
+                </p>
+            </div>
+        </div>
+    </div>
 </main>
 <?php
 $footer->render();
