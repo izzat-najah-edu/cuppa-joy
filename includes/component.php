350c4203a69d968689693299e9ee21ea9e588c40
Izzat Alsharif <izzat5233@gmail.com>
2023-05-05 01:09:02 +0300

    image overlay render function

--- includes/component.php
+++ includes/component.php
@@ -7,3 +7,12 @@ function renderNavbar(): void {
 function renderFooter(): void {
     require_once "components/footer.php";
 }
+
+function imageOverlay($backgroundImage, $title, $height = "40vh"): string {
+    return <<<OVERLAY
+        <div class="image-overlay d-flex align-items-center justify-content-center text-center"
+             style="background-image: url($backgroundImage); height: $height;">
+            <h2 class="card-title display-2 font-title">$title</h2>
+        </div>
+    OVERLAY;
+}
