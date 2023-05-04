52004b52199f1ef1e03178a4a5a78f80218eb5f7
Izzat Alsharif <izzat5233@gmail.com>
2023-05-05 01:10:14 +0300

    refactoring

--- includes/component.php
+++ includes/component.php
@@ -8,10 +8,10 @@ function renderFooter(): void {
     require_once "components/footer.php";
 }
 
-function imageOverlay($backgroundImage, $title, $height = "40vh"): string {
+function imageOverlay($backgroundImageURL, $title, $height = "40vh"): string {
     return <<<OVERLAY
         <div class="image-overlay d-flex align-items-center justify-content-center text-center"
-             style="background-image: url($backgroundImage); height: $height;">
+             style="background-image: url($backgroundImageURL); height: $height;">
             <h2 class="card-title display-2 font-title">$title</h2>
         </div>
     OVERLAY;
