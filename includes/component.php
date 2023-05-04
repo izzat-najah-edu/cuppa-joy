3c449bbad9bb60df7af0d4e96c5f40f5c6569cdb
Izzat Alsharif <izzat5233@gmail.com>
2023-05-05 01:11:41 +0300

    margin fix

--- includes/component.php
+++ includes/component.php
@@ -10,7 +10,7 @@ function renderFooter(): void {
 
 function imageOverlay($backgroundImageURL, $title, $height = "40vh"): string {
     return <<<OVERLAY
-        <div class="image-overlay d-flex align-items-center justify-content-center text-center"
+        <div class="image-overlay d-flex align-items-center justify-content-center text-center mb-5"
              style="background-image: url($backgroundImageURL); height: $height;">
             <h2 class="card-title display-2 font-title">$title</h2>
         </div>
