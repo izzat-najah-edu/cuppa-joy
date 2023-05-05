dc861223ee1e3f85809e3af5f514c2b60dd52de6
Izzat Alsharif <izzat5233@gmail.com>
2023-05-05 21:15:54 +0300

    navbar icons

--- includes/components/navbar.php
+++ includes/components/navbar.php
@@ -6,31 +6,40 @@
         <button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarContent">
             <i class="navbar-toggler-icon"></i>
         </button>
-    </div>
-    <div id="navbarContent" class="collapse navbar-collapse">
-        <ul class="navbar-nav">
-            <li class="nav-item">
-                <a class="nav-link" href="index">
-                    <h1>Home</h1>
+        <div id="navbarContent" class="collapse navbar-collapse">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="index">
+                        <h1>Home</h1>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="menu">
+                        <h1>Menu</h1>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="about">
+                        <h1>About</h1>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="contact">
+                        <h1>Contact</h1>
+                    </a>
+                </li>
+            </ul>
+            <div class="d-flex align-items-center justify-content-end ms-auto">
+                <a class="mx-2" href="#">
+                    <img src="assets/images/icons/language_black.png" alt="Language" width="25px" height="25px">
                 </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="menu">
-                    <h1>Menu</h1>
+                <a class="mx-2" href="#">
+                    <img src="assets/images/icons/theme_black.png" alt="Theme" width="25px" height="25px">
                 </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="about">
-                    <h1>About</h1>
+                <a class="mx-2" href="#">
+                    <img src="assets/images/icons/cart_black.png" alt="Cart" width="25px" height="25px">
                 </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="contact">
-                    <h1>Contact</h1>
-                </a>
-            </li>
-        </ul>
-    </div>
-    <div class="container-fluid">
+            </div>
+        </div>
     </div>
 </nav>
