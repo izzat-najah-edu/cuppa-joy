09018f0c9e835443780573b4475e756f51072794
Izzat Alsharif <izzat5233@gmail.com>
2023-05-05 13:46:25 +0300

    fix

--- includes/component.php
+++ includes/component.php
@@ -8,11 +8,12 @@ function renderFooter(): void {
     require_once "components/footer.php";
 }
 
-function imageOverlay($backgroundImageURL, $title, $height = "40vh"): string {
+function imageOverlay($backgroundImageURL, $title, $height = "60vh", $content = ""): string {
     return <<<OVERLAY
         <div class="image-overlay d-flex align-items-center justify-content-center text-center mb-5"
              style="background-image: url($backgroundImageURL); height: $height;">
             <h2 class="card-title display-2 font-title">$title</h2>
+            $content
         </div>
     OVERLAY;
 }
