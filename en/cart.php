b4b399e63d2a0659994aeee3c63bc9ba895f704b
Izzat Alsharif <izzat5233@gmail.com>
2023-05-13 19:48:42 +0300

    refactoring

--- en/cart.php
+++ en/cart.php
@@ -18,7 +18,7 @@ require_once "../includes/config.php";
 </head>
 <body>
 <header>
-    <?php renderNavbar() ?>
+    <?php require_once "components/navbar.php" ?>
 </header>
 <main>
     <section class="mb-5">
@@ -104,7 +104,7 @@ require_once "../includes/config.php";
         </div>
     </div>
 </main>
-<?php renderFooter() ?>
+<?php require_once "components/footer.php" ?>
 <script src="../assets/js/main.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
--- en/index.php
+++ en/index.php
@@ -15,7 +15,7 @@ require_once "../includes/component.php";
 </head>
 <body>
 <header>
-    <?php renderNavbar() ?>
+    <?php require_once "components/navbar.php" ?>
 </header>
 <main>
     <section class="mb-5">
@@ -80,7 +80,7 @@ require_once "../includes/component.php";
     </section>
     <hr>
 </main>
-<?php renderFooter() ?>
+<?php require_once "components/footer.php" ?>
 <script src="../assets/js/main.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
