d099aac041fbb10922a9cfebd278fe36f6738254
Izzat Alsharif <izzat5233@gmail.com>
2023-05-13 23:32:57 +0300

    fix

---
+++ ar/contact.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <script>
+        location.replace("../en/contact");
+    </script>
+    <title>Redirecting</title>
+</head>
+<body>
+Redirecting...
+</body>
+</html>
