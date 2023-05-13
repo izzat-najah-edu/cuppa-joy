c294ec53479ff88934f1aa2aebde461689720f96
Izzat Alsharif <izzat5233@gmail.com>
2023-05-13 19:58:51 +0300

    arabic components

---
+++ ar/components/footer.php
@@ -0,0 +1,41 @@
+<footer class="bg-dark text-white py-4 mt-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <h5 class="footer-title">كابا جوي</h5>
+                <p>تم التصميم بواسطة سليمان و عزت</p>
+                <ul>
+                    <li><a class="text-white text-decoration-none"
+                           href="mailto://izzat5233@gmail.com">izzat5233@gmail.com</a>
+                    </li>
+                    <li><a class="text-white text-decoration-none"
+                           href="mailto://sualiman@gmail.com">sualiman@gmail.com</a></li>
+                </ul>
+            </div>
+            <div class="col-md-4">
+                <h5 class="footer-title mb-3">روابط سريعة</h5>
+                <ul>
+                    <li><a href="index" class="text-white text-decoration-none">الرئيسية</a></li>
+                    <li><a href="menu" class="text-white text-decoration-none">القائمة</a></li>
+                    <li><a href="about" class="text-white text-decoration-none">حول</a></li>
+                    <li><a href="contact" class="text-white text-decoration-none">اتصل بنا</a></li>
+                </ul>
+            </div>
+            <div class="col-md-4">
+                <h5 class="footer-title mb-3">ابقى على تواصل</h5>
+                <p>تابعنا على وسائل التواصل الاجتماعي للتحديثات والعروض الترويجية.</p>
+                <a href="https://www.facebook.com" class="text-white text-decoration-none me-2"><i
+                        class="bi bi-facebook"></i></a>
+                <a href="https://www.twitter.com" class="text-white text-decoration-none me-2"><i
+                        class="bi bi-twitter"></i></a>
+                <a href="https://www.instagram.com" class="text-white text-decoration-none me-2"><i
+                        class="bi bi-instagram"></i></a>
+            </div>
+        </div>
+        <div class="row mt-3">
+            <div class="col text-center">
+                <p class="mb-0">&copy; <?php echo date('Y'); ?> كابا جوي. جميع الحقوق محفوظة.</p>
+            </div>
+        </div>
+    </div>
+</footer>
---
+++ ar/components/navbar.php
@@ -0,0 +1,45 @@
+<nav class="navbar navbar-expand-xl navbar-light px-5">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="index">
+            <img class="img-fluid" src="../assets/images/icons/logo_dark.png" alt="Logo">
+        </a>
+        <button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarContent">
+            <i class="navbar-toggler-icon"></i>
+        </button>
+        <div id="navbarContent" class="collapse navbar-collapse">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="index">
+                        <h3>الرئيسية</h3>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="menu">
+                        <h3>القائمة</h3>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="about">
+                        <h3>من نحن</h3>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="contact">
+                        <h3>تواصل معنا</h3>
+                    </a>
+                </li>
+            </ul>
+            <div class="d-flex align-items-center justify-content-end ms-auto">
+                <i class="mx-2 btn" onclick="toggleLanguage()">
+                    <img src="../assets/images/icons/language_dark.png" alt="Language" width="25px" height="25px">
+                </i>
+                <i class="mx-2 btn" onclick="toggleTheme()">
+                    <img src="../assets/images/icons/theme_dark.png" alt="Theme" width="25px" height="25px">
+                </i>
+                <a class="mx-2 btn" href="cart">
+                    <img src="../assets/images/icons/cart_dark.png" alt="Cart" width="25px" height="25px">
+                </a>
+            </div>
+        </div>
+    </div>
+</nav>
