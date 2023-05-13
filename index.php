5f3f930969df78ab83b908695b6dded801f92adf
Izzat Alsharif <izzat5233@gmail.com>
2023-05-13 14:57:23 +0300

    language redirecting entry

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <script>
+        location.href = localStorage.getItem("lang") || "en";
+    </script>
+    <title>Redirecting</title>
+</head>
+<body>
+<p>Redirecting you to your preferred language...</p>
+<noscript>
+    <p>If you are not redirected, please select your language:</p>
+    <ul>
+        <li><a href="en">English</a></li>
+        <li><a href="ar">Arabic</a></li>
+    </ul>
+</noscript>
+</body>
+</html>
