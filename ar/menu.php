83adfb3e0422f6d22e81a4814ac33cd6d40c8082
Izzat Alsharif <izzat5233@gmail.com>
2023-05-14 13:06:54 +0300

    refactoring

--- ar/menu.php
+++ ar/menu.php
@@ -21,7 +21,7 @@ require_once "../actions/config.php";
 </head>
 <body>
 <header>
-    <?php require_once "components/navbar.php" ?>
+    <?php require_once "includes/navbar.php" ?>
 </header>
 <main>
     <section class="mb-5">
@@ -99,7 +99,7 @@ require_once "../actions/config.php";
         </div>
     </section>
 </main>
-<?php require_once "components/footer.php" ?>
+<?php require_once "includes/footer.php" ?>
 <script src="../assets/js/main.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
