a0c171cf82b28f036e8d24df12ce8d791ae91c2f
Izzat Alsharif <izzat5233@gmail.com>
2023-05-15 16:33:50 +0300

    fix

--- sql/CREATE.sql
+++ sql/CREATE.sql
@@ -32,7 +32,7 @@ CREATE TABLE messages
 
 CREATE TABLE `admin`
 (
-    id         INT PRIMARY KEY,
+    id         INT AUTO_INCREMENT PRIMARY KEY,
     username   VARCHAR(255) NOT NULL UNIQUE,
     password   VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
