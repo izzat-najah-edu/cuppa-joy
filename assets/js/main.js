72d7669ed198676fcc7713c4250fb17d9ee4fa60
Izzat Alsharif <izzat5233@gmail.com>
2023-05-16 00:46:14 +0300

    refactoring

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,47 +1,9 @@
-function setCookie(name, value, days) {
-    let expires = "";
-    if (days) {
-        let date = new Date();
-        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-        expires = "; expires=" + date.toUTCString();
+document.addEventListener("DOMContentLoaded", function setupTheme() {
+    if (localStorage.getItem("theme") && localStorage.getItem("theme") === "dark") {
+        localStorage.setItem("theme", "light");
+        toggleTheme();
     }
-    document.cookie = name + "=" + encodeURIComponent(value || "") + expires + "; path=/";
-}
-
-function getCookie(name) {
-    const cookiesArray = document.cookie.split(";");
-    for (let cookie of cookiesArray) {
-        cookie = cookie.trim();
-        if (cookie.indexOf(name) === 0) {
-            return decodeURIComponent(cookie.substring(name.length + 1));
-        }
-    }
-    return null;
-}
-
-function requestAction(action, body, onload) {
-    const xhr = new XMLHttpRequest();
-    xhr.open("post", `../actions/${action}.php`, true);
-    xhr.onload = function () {
-        if (this.status !== 200) {
-            console.log("Request failed");
-            return;
-        }
-        let response = JSON.parse(this.responseText);
-        if (!response.success) {
-            console.log(response.message);
-            return;
-        }
-        if (onload) {
-            onload(response);
-        }
-    }
-    xhr.send(body);
-}
-
-function showModal(modal) {
-    new bootstrap.Modal(modal, {}).show();
-}
+});
 
 function toggleTheme() {
     const theme = localStorage.getItem("theme") === "light" ? "light" : "dark";
@@ -82,14 +44,31 @@ function toggleThemeButtons() {
     })
 }
 
-document.addEventListener("DOMContentLoaded", function setupTheme() {
-    if (localStorage.getItem("theme") && localStorage.getItem("theme") === "dark") {
-        localStorage.setItem("theme", "light");
-        toggleTheme();
-    }
-});
-
 function toggleLanguage(newLanguage) {
     localStorage.setItem("lang", newLanguage ?? "en");
     location.href = ".."
+}
+
+function showModal(modal) {
+    new bootstrap.Modal(modal, {}).show();
+}
+
+function requestAction(action, body, onload) {
+    const xhr = new XMLHttpRequest();
+    xhr.open("post", `../actions/${action}.php`, true);
+    xhr.onload = function () {
+        if (this.status !== 200) {
+            console.log("Request failed");
+            return;
+        }
+        let response = JSON.parse(this.responseText);
+        if (!response.success) {
+            console.log(response.message);
+            return;
+        }
+        if (onload) {
+            onload(response);
+        }
+    }
+    xhr.send(body);
 }
