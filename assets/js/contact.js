45c126240d631908b18bc67e2cc877c2769d69a4
Izzat Alsharif <izzat5233@gmail.com>
2023-05-18 10:38:47 +0300

    images compression

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -7,34 +7,30 @@ document.getElementById("message-form").addEventListener("submit", function (eve
     );
 });
 
-function setUpContactThemeToggle() {
-    const inputs = document.querySelectorAll('.contact-input');
-    const toggleBtn = document.querySelector(".theme-toggle");
-    const allElements = document.querySelectorAll("*");
-
-    toggleBtn.addEventListener("click", () => {
-        document.body.classList.toggle("dark");
-        // toggleBtn.classList.toggle("bx-sun");
-        allElements.forEach((el) => {
-            el.classList.add("transition");
-            setTimeout(() => {
-                el.classList.remove("transition");
-            }, 1000);
-        });
+function toggleTheme() {
+    document.body.classList.toggle("dark");
+    // toggleBtn.classList.toggle("bx-sun");
+    document.querySelectorAll("*").forEach((el) => {
+        el.classList.add("transition");
+        setTimeout(() => {
+            el.classList.remove("transition");
+        }, 1000);
     });
-
-    inputs.forEach((ipt) => {
-        ipt.addEventListener('focus', () => {
-            ipt.parentNode.classList.add("focus");
-            ipt.parentNode.classList.add("not-empty");
-        });
-        ipt.addEventListener('blur', () => {
-            if (ipt.value === "") {
-                ipt.parentNode.classList.remove("not-empty");
-            }
-            ipt.parentNode.classList.remove("focus");
-        });
-    })
 }
 
-setUpContactThemeToggle();
+document.addEventListener("DOMContentLoaded", toggleTheme);
+
+document.querySelector(".theme-toggle").addEventListener("click", toggleTheme);
+
+document.querySelectorAll('.contact-input').forEach((ipt) => {
+    ipt.addEventListener('focus', () => {
+        ipt.parentNode.classList.add("focus");
+        ipt.parentNode.classList.add("not-empty");
+    });
+    ipt.addEventListener('blur', () => {
+        if (ipt.value === "") {
+            ipt.parentNode.classList.remove("not-empty");
+        }
+        ipt.parentNode.classList.remove("focus");
+    });
+});
