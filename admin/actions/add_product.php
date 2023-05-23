bf6530455d6bc32751ea910691160d4997b7e4d6
Izzat Alsharif <izzat5233@gmail.com>
2023-05-23 23:27:32 +0300

    create product action

--- admin/actions/add_product.php
+++ admin/actions/add_product.php
@@ -1,3 +1,29 @@
 <?php
 require_once "../lock.php";
 require_once "../../includes/config.php";
+
+if ($_SERVER["REQUEST_METHOD"] !== "POST") {
+    header("Location: ../index.php");
+    exit;
+}
+
+if (!isset($_POST["name"]) ||
+    !isset($_POST["description"]) ||
+    !isset($_POST["price"]) ||
+    !isset($_POST["image-url"]) ||
+    !isset($_POST["name-arabic"]) ||
+    !isset($_POST["description-arabic"]) ||
+    !Database::getInstance()->insertCoffee(
+        $_POST["name"],
+        $_POST["description"],
+        $_POST["price"],
+        $_POST["image-url"],
+        $_POST["name-arabic"],
+        $_POST["description-arabic"])) {
+    $_SESSION["insert_message"] = "Invalid product information!";
+} else {
+    $_SESSION["insert_message"] = "Product [{$_POST['name']}] has been created successfully!";
+}
+
+header("Location: ../dashboard.php");
+exit;
