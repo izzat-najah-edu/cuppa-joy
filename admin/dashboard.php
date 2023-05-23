6a7b11be1b37a440c03683824907da006b8a9cb6
Izzat Alsharif <izzat5233@gmail.com>
2023-05-23 23:48:52 +0300

    fix

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -38,23 +38,19 @@ require_once "../includes/config.php";
             <form action="actions/add_product.php" method="post" class="p-3 rounded">
                 <div class="form-group">
                     <label for="insert_name">Name:</label>
-                    <input type="text" class="form-control" id="insert_name" name="name"
-                           placeholder="Product Name" required>
+                    <input type="text" class="form-control" id="insert_name" name="name" required>
                 </div>
                 <div class="form-group">
                     <label for="insert_description">Description:</label>
-                    <textarea class="form-control" id="insert_description" name="description"
-                              placeholder="Product Description" required></textarea>
+                    <textarea class="form-control" id="insert_description" name="description" required></textarea>
                 </div>
                 <div class="form-group">
                     <label for="insert_price">Price:</label>
-                    <input type="number" step="0.01" class="form-control" id="insert_price" name="price"
-                           placeholder="Product Price" required>
+                    <input type="number" step="0.01" class="form-control" id="insert_price" name="price" required>
                 </div>
                 <div class="form-group">
-                    <label for="insert_imageURL">Image URL:</label>
-                    <input type="text" class="form-control" id="insert_imageURL" name="image-url"
-                           placeholder="Product Image [e.g. 'images/coffee/name.jpg']" required>
+                    <label for="insert_imageURL">Image URL: [e.g. 'images/coffee/name.jpg']</label>
+                    <input type="text" class="form-control" id="insert_imageURL" name="image-url" required>
                 </div>
                 <div class="form-group">
                     <button type="submit" class="btn btn-primary">Add Coffee</button>
