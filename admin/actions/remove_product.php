beb6faaad98ba612020b5c338c73335dd1b296be
Izzat Alsharif <izzat5233@gmail.com>
2023-05-23 23:17:42 +0300

    delete action

--- admin/actions/remove_product.php
+++ admin/actions/remove_product.php
@@ -1,3 +1,18 @@
 <?php
 require_once "../lock.php";
 require_once "../../includes/config.php";
+
+if ($_SERVER["REQUEST_METHOD"] !== "POST") {
+    header("Location: ../index.php");
+    exit;
+}
+
+if (!isset($_POST["name"]) ||
+    !Database::getInstance()->deleteCoffee($_POST["name"])) {
+    $_SESSION["delete_message"] = "Invalid Product Name!";
+} else {
+    $_SESSION["delete_message"] = "Product Has Been Deleted Successfully!";
+}
+
+header("Location: ../dashboard.php");
+exit;
